199f08f8d3e8e6b7e5dbdd9b84a69a0152eeeace
serranojc15 <serranojc15@gmail.com>
2026-07-27 15:27:48 -0500

    Bump release for order reveal encouragement polish

--- release.js
+++ release.js
@@ -4,8 +4,8 @@
 (function (root) {
   "use strict";
 
-  const VERSION = "v0.4.4.9";
-  const RELEASE_NAME = "Restaurant Prediction Flow Polish";
+  const VERSION = "v0.4.5.0";
+  const RELEASE_NAME = "Order Reveal Encouragement Polish";
   const DISPLAY_LABEL = `Bite Buddy League · ${VERSION}`;
 
   function each(selector, callback) {
