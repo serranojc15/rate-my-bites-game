f82c92a606c9b32c71eb854312b34356f6cebc4a
serranojc15 <serranojc15@gmail.com>
2026-07-27 15:56:59 -0500

    Run Sprint 4.4.3 validation release-neutrally

---
+++ tests/sprint443ReleaseNeutral.test.cjs
@@ -0,0 +1,16 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const Module = require('node:module');
+
+const testPath = path.resolve('tests/sprint443.test.cjs');
+let testSource = fs.readFileSync(testPath, 'utf8');
+
+testSource = testSource.replace(
+  "ok(/^v0\\.4\\.4\\.\\d+$/.test(releaseVersion), 'current release is Sprint 4.4.3 or later');",
+  "ok(require('./version-helpers.cjs').isVersionAtLeast(releaseVersion, 'v0.4.4.3'), 'current release is Sprint 4.4.3 or later');"
+);
+
+const compiled = new Module(testPath, module);
+compiled.filename = testPath;
+compiled.paths = Module._nodeModulePaths(process.cwd());
+compiled._compile(testSource, testPath);
